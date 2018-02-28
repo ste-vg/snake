25c3e4ea06e3cb70a2280d6eaeb498db6a9f71d2
Steve Gardner <github@ste.vg>
2018-02-28 08:40:49 -0800

    1.0.3

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,18 @@ import '../styles.scss';
 import './app.scss';
 
 import { Observable, Subscription } from "rxjs";
+import { Snake } from "./snake";
 
 declare function require(moduleName: string): any;
 const {version : appVersion} = require('../../package.json');
 
-import { Snake } from "./snake";
-
-let html = require('./app.html');
+const html = require('./app.html');
 
 export class App
 {
     private game:Snake;
     private score:HTMLElement;
+    private container:HTMLElement;
     private gameState:string;
 
     constructor(container:HTMLElement)
@@ -26,20 +26,23 @@ export class App
     setupUI()
     {
         this.score = document.getElementById('score');
-        let startButton = Observable.fromEvent(document.getElementById('startButton'), 'click');
+        this.container = document.getElementById('container');
+        let startButton = Observable.fromEvent(document.getElementById('start-button'), 'click');
         startButton.subscribe((e:MouseEvent) => { this.startGame(); })
-
         document.getElementById('app-version').innerHTML = appVersion;
     }
 
     setupGame()
     {
         let board = document.getElementById('board');
+
         this.game = new Snake(board);
-
         this.game.score.subscribe((score:number) => this.score.innerHTML = String(score));
-        this.game.state.subscribe((state:string) => this.gameState = state);
-
+        this.game.state.subscribe((state:string) => 
+        {
+            this.gameState = state;
+            this.container.setAttribute('class', this.gameState);
+        })
         this.game.reset();
     }
 
